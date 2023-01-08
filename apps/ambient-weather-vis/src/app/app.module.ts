b1b5054b90be3857095c1601c9a3ea78d40ed92d
Sanjeev <sanjeev.b@gmx.de>
2023-01-08 14:55:24 +0100

    - add toolbar of angular material

--- apps/ambient-weather-vis/src/app/app.module.ts
+++ apps/ambient-weather-vis/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './basic/header/header.component';
+import { SharedModule } from "./shared/shared.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/ambient-weather-vis/src/app/basic/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ambient-weather-vis-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass'],
+})
+export class HeaderComponent {
+}
