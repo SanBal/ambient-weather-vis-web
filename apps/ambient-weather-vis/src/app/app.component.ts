489eee4280f0fadc5fca725d64294e39e0eac9b8
Sanjeev <sanjeev.b@gmx.de>
2023-01-08 13:48:55 +0100

    - fix title and selector

--- apps/ambient-weather-vis/src/app/app.component.ts
+++ apps/ambient-weather-vis/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ambient-weather-vis-web-root',
+  selector: 'ambient-weather-vis',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
